382b69a0c99bf762102311a6b16ba124473ff6b2
coffmanjrp <coffmanjrp@gmail.com>
2020-12-26 17:20:13 -0800

    first commit

---
+++ dist/js/main.js
@@ -0,0 +1,28 @@
+"use strict";
+var sliderContainer = document.querySelector('.slider-container');
+var slideRight = document.querySelector('.right-slide');
+var slideLeft = document.querySelector('.left-slide');
+var upButton = document.querySelector('.up-button');
+var downButton = document.querySelector('.down-button');
+var slidesLength = slideRight.querySelectorAll('div').length;
+var activeSlideIndex = 0;
+slideLeft.style.top = "-" + [slidesLength - 1] * 100 + "vh";
+var changeSlide = function (direction) {
+    var sliderHeight = sliderContainer.clientHeight;
+    if (direction === 'up') {
+        activeSlideIndex++;
+        if (activeSlideIndex > slidesLength - 1) {
+            activeSlideIndex = 0;
+        }
+    }
+    else if (direction === 'down') {
+        activeSlideIndex--;
+        if (activeSlideIndex < 0) {
+            activeSlideIndex = slidesLength - 1;
+        }
+    }
+    slideRight.style.transform = "translateY(-" + activeSlideIndex * sliderHeight + "px)";
+    slideLeft.style.transform = "translateY(" + activeSlideIndex * sliderHeight + "px)";
+};
+upButton.addEventListener('click', function () { return changeSlide('up'); });
+downButton.addEventListener('click', function () { return changeSlide('down'); });
---
+++ src/ts/main.ts
@@ -0,0 +1,38 @@
+const sliderContainer = document.querySelector(
+  '.slider-container'
+) as HTMLElement;
+const slideRight = document.querySelector('.right-slide') as HTMLElement;
+const slideLeft = document.querySelector('.left-slide') as HTMLElement;
+const upButton = document.querySelector('.up-button') as HTMLElement;
+const downButton = document.querySelector('.down-button') as HTMLElement;
+const slidesLength: number = slideRight.querySelectorAll('div').length;
+
+let activeSlideIndex = 0;
+
+slideLeft.style.top = `-${[slidesLength - 1] * 100}vh`;
+
+const changeSlide = (direction: string) => {
+  const sliderHeight = sliderContainer.clientHeight;
+
+  if (direction === 'up') {
+    activeSlideIndex++;
+    if (activeSlideIndex > slidesLength - 1) {
+      activeSlideIndex = 0;
+    }
+  } else if (direction === 'down') {
+    activeSlideIndex--;
+    if (activeSlideIndex < 0) {
+      activeSlideIndex = slidesLength - 1;
+    }
+  }
+
+  slideRight.style.transform = `translateY(-${
+    activeSlideIndex * sliderHeight
+  }px)`;
+  slideLeft.style.transform = `translateY(${
+    activeSlideIndex * sliderHeight
+  }px)`;
+};
+
+upButton.addEventListener('click', () => changeSlide('up'));
+downButton.addEventListener('click', () => changeSlide('down'));
